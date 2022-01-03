97f9e785bb4fc10ec7f03155adc95a10cffcdeba
zjl9959 <zjl9959@gmail.com>
2022-01-03 19:13:58 +0800

    Update test framework, add TestResult class.

---
+++ tests/result.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+'''
+@author:zjl9959@gmail.com
+@license:GPLv3
+'''
+
+
+class TestResult:
+    def __init__(self) -> None:
+        self._nb_pass = 0
+        self._nb_failed = 0
+        self._failed_cases = dict()
+
+    def __repr__(self) -> str:
+        str = "  - Total cases:{0}, Passed cases:{1}, Failed cases:{2}\r\n".format(
+            self._nb_pass + self._nb_failed,
+            self._nb_pass,
+            self._nb_failed
+            )
+        for case, desc in self._failed_cases.items():
+            str += "   - Case:{0}, Expect:{1}, Actual:{2}\r\n".format(case, desc[1], desc[0])
+        return str
+
+    def add_case(self, ok, case_name=None, test_result=None, expect_result=None):
+        if ok:
+            self._nb_pass = self._nb_pass + 1
+        else:
+            self._nb_failed = self._nb_failed + 1
+            if case_name:
+                self._failed_cases[case_name] = (test_result, expect_result)
+
+    def ok(self):
+        return self._nb_pass > 0 and self._nb_failed == 0
--- tests/run.py
+++ tests/run.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+
+'''
+@author:zjl9959@gmail.com
+@license:GPLv3
+'''
+
+import sys
+import os
+sys.path.append(os.path.join(sys.path[0], "../algviz"))
+
+
+def main():
+    raise('not implement error.')
+
+
+def print_test_results(results):
+    num_pass, num_failed = 0, 0
+    for result in results.values():
+        if result.ok():
+            num_pass = num_pass + 1
+        else:
+            num_failed = num_failed + 1
+    print("- Total functions:{0:>9}\r\n- Passed functions:{1:>8}\r\n- Failed functions:{2:>8}".format(
+            len(results), num_pass, num_failed))
+    for func, result in results.items():
+        if not result.ok():
+            print(" - {0}".format(func))
+            print(result)
+
+
+if __name__ == '__main__':
+    main()
