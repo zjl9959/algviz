edf25acbfce901636b051e188d8e459ab3a05455
zjl9959 <zjl9959@gmail.com>
2022-01-16 17:48:54 +0800

    [Test] Add test cases for DoublyLinkedList.

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -109,23 +109,141 @@ def test_modify_forward_linked_list():
     nodes, edges = get_graph_elements(graph._repr_svg_())
     res.add_case(equal(nodes, expect_nodes) and equal_table(edges, expect_edges), 'Reverse list',
                 'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(expect_nodes, expect_edges))
-    res.add_case(False, 'dsds')
     return res
 
 
 def test_create_doubly_linked_list():
     res = TestResult()
-    # TODO: Add subcases.
+    viz = algviz.Visualizer()
+    list_info = [1, 2, 3, 4, 'tail']
+    head, tail = algviz.parseDoublyLinkedList(list_info)
+    # Test init graph by head node.
+    graph = viz.createGraph(head)
+    nodes, edges = get_graph_elements(graph._repr_svg_())
+    expect_edges = [
+        (1, 2, None), (2, 1, None),
+        (2, 3, None), (3, 2, None),
+        (3, 4, None), (4, 3, None),
+        (4, 'tail', None), ('tail', 4, None)
+    ]
+    res.add_case(equal(nodes, list_info) and equal_table(edges, expect_edges), 'Create list(head)',
+                'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(list_info, expect_edges))
+    # Test init graph by tail node.
+    graph = viz.createGraph(tail)
+    nodes, edges = get_graph_elements(graph._repr_svg_())
+    res.add_case(equal(nodes, list_info) and equal_table(edges, expect_edges), 'Create list(tail)',
+                'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(list_info, expect_edges))
     return res
 
 
 def test_traverse_doubly_linked_list():
     res = TestResult()
-    # TODO: Add subcases.
+    viz = algviz.Visualizer()
+    list_info = [1, 2, 3, 4, 5, 6]
+    head, tail = algviz.parseDoublyLinkedList(list_info)
+    graph = viz.createGraph(head)
+    # Test visit list node in forward direction.
+    cur, visit_list = head, list()
+    while cur:
+        visit_list.append(cur.val)
+        cur.val += 1
+        cur = cur.next
+    res.add_case(equal(visit_list, list_info), 'Visit list(forward)',
+                visit_list, list_info)
+    # Test update list node value.
+    nodes, _ = get_graph_elements(graph._repr_svg_())
+    expect_list = [2, 3, 4, 5, 6, 7]
+    res.add_case(equal(nodes, expect_list), 'Update node value',
+                nodes, expect_list)
+    # Test visit list node in backward direction.
+    cur, visit_list = tail, list()
+    while cur:
+        visit_list.append(cur.val)
+        cur = cur.prev
+    expect_list = [7, 6, 5, 4, 3, 2]
+    res.add_case(equal(visit_list, expect_list), 'Visit list(backward)',
+                visit_list, expect_list)
     return res
 
 
 def test_modify_doubly_linked_list():
     res = TestResult()
-    # TODO: Add subcases.
+    viz = algviz.Visualizer()
+    list_info = [2, 4]
+    node2, node4 = algviz.parseDoublyLinkedList(list_info)
+    graph = viz.createGraph(node2)
+    # Test add nodes into doubly linked list.
+    node1 = algviz.DoublyLinkedListNode(1)
+    node3 = algviz.DoublyLinkedListNode(3)
+    node5 = algviz.DoublyLinkedListNode(5)
+    node1.next = node2; node2.prev = node1; graph.addNode(node1)
+    node2.next = node3; node3.prev = node2; node3.next = node4
+    node4.next = node5; node4.prev = node3; node5.prev = node4
+    expect_nodes = [1, 2, 3, 4, 5]
+    expect_edges = [
+        (1, 2, None), (2, 1, None),
+        (2, 3, None), (3, 2, None),
+        (3, 4, None), (4, 3, None),
+        (4, 5, None), (5, 4, None)
+    ]                   # 1<->2<->3<->4<->5
+    graph._repr_svg_()  # Skip the animation frame.
+    nodes, edges = get_graph_elements(graph._repr_svg_())
+    res.add_case(equal(nodes, expect_nodes) and equal_table(edges, expect_edges), 'Add nodes',
+                'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(expect_nodes, expect_edges))
+    # Test remove nodes from doubly linked list.
+    node1.next = node3; node3.prev = node1
+    node2.next = None; node2.prev = None
+    node3.next = node5; node5.prev = node3
+    node4.next = None; node4.prev = None
+    graph.removeNode(node2); graph.removeNode(node4)
+    expect_nodes = [1, 3, 5]
+    expect_edges = [
+        (1, 3, None), (3, 1, None),
+        (3, 5, None), (5, 3, None),
+    ]                   # 1<->3<->5
+    graph._repr_svg_()  # Skip the animation frame.
+    nodes, edges = get_graph_elements(graph._repr_svg_())
+    res.add_case(equal(nodes, expect_nodes) and equal_table(edges, expect_edges), 'Remove nodes',
+                'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(expect_nodes, expect_edges))
+    return res
+
+
+def test_two_forward_linked_lists():
+    res = TestResult()
+    viz = algviz.Visualizer()
+    list1_info = [1, 2, 3]; list2_info = [4, 5, 6]
+    list1 = algviz.parseForwardLinkedList(list1_info)
+    graph = viz.createGraph(list1)
+    list2 = algviz.parseForwardLinkedList(list2_info)
+    list1.next.next.next = list2
+    graph._repr_svg_()
+    nodes, edges = get_graph_elements(graph._repr_svg_())
+    expect_nodes = [1, 2, 3, 4, 5, 6]
+    expect_edges = [(1, 2, None), (2, 3, None), (3, 4, None), (4, 5, None), (5, 6, None)]
+    res.add_case(equal(nodes, expect_nodes) and equal_table(edges, expect_edges), 'Link two lists',
+                'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(expect_nodes, expect_edges))
+    return res
+
+
+def test_two_doubly_linked_lists():
+    res = TestResult()
+    viz = algviz.Visualizer()
+    list1_info = [1, 2, 3]; list2_info = [4, 5, 6]
+    list1, tail1 = algviz.parseDoublyLinkedList(list1_info)
+    graph = viz.createGraph(list1)
+    list2, _ = algviz.parseDoublyLinkedList(list2_info)
+    tail1.next = list2
+    list2.prev = tail1
+    graph._repr_svg_()
+    nodes, edges = get_graph_elements(graph._repr_svg_())
+    expect_nodes = [1, 2, 3, 4, 5, 6]
+    expect_edges = [
+        (1, 2, None), (2, 1, None),
+        (2, 3, None), (3, 2, None),
+        (3, 4, None), (4, 3, None),
+        (4, 5, None), (5, 4, None),
+        (5, 6, None), (6, 5, None)
+    ]
+    res.add_case(equal(nodes, expect_nodes) and equal_table(edges, expect_edges), 'Link two lists',
+                'nodes:{};edges:{}'.format(nodes, edges), 'nodes:{};edges:{}'.format(expect_nodes, expect_edges))
     return res
