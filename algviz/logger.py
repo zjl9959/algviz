1cba9cbf9507bd085852d59783400e8e64082fa4
zjl9959 <zjl9959@gmail.com>
2022-05-22 17:04:34 +0800

    Revert "Add _repr_svg_ interface for Logger."
    This reverts commit 0e261891f8e6fcc0a6c9e55eabcb266d6c60b6f2.

--- algviz/logger.py
+++ algviz/logger.py
@@ -8,11 +8,6 @@ License: GPLv3
 
 """
 
-from algviz.utility import get_text_width
-
-from xml.dom.minidom import Document
-
-
 class Logger():
     """A log class to display strings into the screen.
 
@@ -20,21 +15,13 @@ class Logger():
     And if the buffer lines overflow, the oldest lines in the buffer will be discard.
     """
 
-    def __init__(self, buffer_lines, font_size=12):
+    def __init__(self, buffer_lines):
         """
         Args:
             buffer_lines (int): Set the max buffer lines for cached logs. Override the oldest line if overflow.
         """
         self._buffer_lines = max(buffer_lines, 1)
-        font_size = int(font_size)
-        self._font_size = font_size if font_size > 0 else 16
         self._logs = list()
-        self._log_text_nodes = list()
-        self._log_text_width = list()
-        self._dom = Document()
-        self._svg = self._dom.createElement('svg')
-        self._svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
-        self._dom.appendChild(self._svg)
     
 
     def write(self, data):
@@ -47,11 +34,6 @@ class Logger():
         for line in data_lines:
             if len(self._logs) >= self._buffer_lines:
                 self._logs.pop(0)
-                if len(self._log_text_width):
-                    self._log_text_width.pop(0)
-                if len(self._log_text_nodes):
-                    txt_node = self._log_text_nodes.pop(0)
-                    self._svg.removeChild(txt_node)
             self._logs.append(line)
     
 
@@ -59,29 +41,10 @@ class Logger():
         """Clear all cached log string.
         """
         self._logs.clear()
-        self._log_text_width.clear()
-        for node in self._log_text_nodes:
-            self._svg.removeChild(node)
-        self._log_text_nodes.clear()
+    
 
-
-    def _repr_svg_(self):
-        for i in range(len(self._log_text_nodes), len(self._logs)):
-            log = self._logs[i]
-            txt = self._dom.createElement('text')
-            txt.setAttribute('x', '10')
-            txt.setAttribute('y', '{:.2f}'.format(self._font_size*(i*1.2+1)))
-            txt.setAttribute('font-size', '{:.2f}'.format(self._font_size))
-            txt.setAttribute('font-family', 'Times,serif')
-            text = self._dom.createTextNode('{}'.format(log))
-            txt.appendChild(text)
-            self._svg.appendChild(txt)
-            self._log_text_width.append(get_text_width(log, self._font_size))
-            self._log_text_nodes.append(txt)
-        # Update svg width and height.
-        svg_width = max(self._log_text_width+[0])*0.6 + 10
-        svg_height = len(self._logs)*self._font_size*1.2
-        self._svg.setAttribute('width', '{:.0f}pt'.format(svg_width))
-        self._svg.setAttribute('height', '{:.0f}pt'.format(svg_height))
-        self._svg.setAttribute('viewBox', '0.00 0.00 {:.2f} {:.2f}'.format(svg_width, svg_height))
-        return self._dom.toxml()
+    def __repr__(self):
+        res = str()
+        for s in self._logs:
+            res += s + '\n'
+        return res
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -5,10 +5,7 @@
 @license: GPLv3
 '''
 
-import xml.dom.minidom as xmldom
-
 from result import TestResult
-from utility import equal
 import algviz
 
 def test_logger():
@@ -17,44 +14,27 @@ def test_logger():
     logger = viz.createLogger(3)
     # Test write line.
     logger.write('line 1')
-    log_str = get_logs_from_svg(logger._repr_svg_())
-    expect_str = ['line 1']
-    res.add_case(equal(log_str, expect_str), 'Write log',
+    log_str = logger.__repr__()
+    expect_str = 'line 1\n'
+    res.add_case(log_str==expect_str, 'Write log',
                 log_str, expect_str)
     # Test multi-line logger.
     logger.write('line 2')
-    log_str = get_logs_from_svg(logger._repr_svg_())
-    expect_str = ['line 1', 'line 2']
-    res.add_case(equal(log_str, expect_str), 'Multi-line logs',
+    log_str = logger.__repr__()
+    expect_str = 'line 1\nline 2\n'
+    res.add_case(log_str==expect_str, 'Multi-line logs',
                 log_str, expect_str)
     # Test logger buffer refresh.
     logger.write('line 3')
     logger.write('line 4')  # Buffer overflow, remove log: 'line 1'.
-    log_str = get_logs_from_svg(logger._repr_svg_())
-    expect_str = ['line 2', 'line 3', 'line 4']
-    res.add_case(equal(log_str, expect_str), 'Buffer refresh',
+    log_str = logger.__repr__()
+    expect_str = 'line 2\nline 3\nline 4\n'
+    res.add_case(log_str==expect_str, 'Buffer refresh',
                 log_str, expect_str)
     # Test clear log.
     logger.clear()
-    log_str = get_logs_from_svg(logger._repr_svg_())
-    expect_str = []
-    res.add_case(equal(log_str, expect_str), 'Clear log',
-                log_str, expect_str)
-    # Test buffer overflow much.
-    logger.write('line 8')
-    logger.write('line 9')
-    logger.write('line 10')
-    log_str = get_logs_from_svg(logger._repr_svg_())
-    expect_str = ['line 8', 'line 9', 'line 10']
-    res.add_case(equal(log_str, expect_str), 'Buffer overflow',
+    log_str = logger.__repr__()
+    expect_str = ''
+    res.add_case(log_str==expect_str, 'Clear log',
                 log_str, expect_str)
     return res
-
-
-def get_logs_from_svg(svg_str):
-    text_lists = list()
-    svg = xmldom.parseString(svg_str)
-    text_nodes = svg.getElementsByTagName('text')
-    for node in text_nodes:
-        text_lists.append(node.firstChild.data)
-    return text_lists
